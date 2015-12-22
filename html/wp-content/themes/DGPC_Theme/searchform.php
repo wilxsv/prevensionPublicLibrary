8572990e84ac20d2c85f6bb792c7bf0706b6a213
Miguel Hernandez <migher@gmail.com>
2015-12-21 23:25:09 -0600

    Actualizacion del template y pluguins de edicion

--- html/wp-content/themes/DGPC_Theme/searchform.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php echo home_url(); ?>/" id="searchform" class="searchform" method="get">
-	<input type="text" id="s" name="s" value="<?php _e('Search', 'envirra') ?>" onfocus="if(this.value=='<?php _e('Search', 'envirra') ?>')this.value='';" onblur="if(this.value=='')this.value='<?php _e('Search', 'envirra') ?>';" autocomplete="off" />
-	<button class="search-button"><i class="icon-entypo-search"></i></button>
-</form>
