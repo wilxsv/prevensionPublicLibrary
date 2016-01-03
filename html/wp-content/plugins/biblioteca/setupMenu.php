d7cf091d2a4ab7a5aecf4494a9011b8cabc755a9
wilxsv <wixl.sv@gmail.com>
2016-01-03 12:58:24 -0600

    core restored

--- html/wp-content/plugins/biblioteca/setupMenu.php
+++
@@ -1,5 +0,0 @@
-<?php
- add_menu_page('Listado de recursos en la biblioteca virtual', 'Biblioteca Virtual', 'manage_options', 'bvirtual', 'listadoHerramienta', 'dashicons-book-alt');
- add_submenu_page('bvirtual', 'Ficha de Biblioteca Virtual', 'Agregar herramienta','manage_options', 'agregaHerramienta','FormHerramientas');
- add_submenu_page('bvirtual', 'Parámetros generales', 'Parametros','manage_options', 'catalogo-herramienta','parametros');
-?>
